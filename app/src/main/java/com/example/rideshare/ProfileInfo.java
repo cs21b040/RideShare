d6d5bf2fc328d6f5a165977f846d9b6fd0401776
“CS21B029” <cs21b029@iittp.ac.in>
2023-08-16 17:06:51 +0530

    "changes in ui"

--- app/src/main/java/com/example/rideshare/ProfileInfo.java
+++ app/src/main/java/com/example/rideshare/ProfileInfo.java
@@ -18,44 +18,25 @@ import android.widget.TextView;
 
 public class ProfileInfo extends AppCompatActivity {
     private EditText dob;
-    private EditText pno;
     private EditText vehicletype;
     private EditText vehiclenumber;
-    private Button browse;
     private Button saving;
-    private TextView uploadlicense;
-    private TextView uploadedornot;
+    private EditText dln;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_info);
         dob=findViewById(R.id.dob);
-        pno=findViewById(R.id.pno);
         vehicletype=findViewById(R.id.vehicletype);
         vehiclenumber=findViewById(R.id.vehiclenumber);
-        browse=findViewById(R.id.browse);
         saving=findViewById(R.id.saving);
-        uploadlicense=findViewById(R.id.uploadlicense);
-        uploadedornot=findViewById(R.id.uploadedornot);
-        browse.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                selectImage();
-            }
-        });
+        dln=findViewById(R.id.dln);
         saving.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(ProfileInfo.this,HomePage.class));
             }
         });
-    }
-
-    private void selectImage() {
-//        Intent intent=new Intent();
-//        intent.setType("image/*");
-//        intent.setAction(Intent.ACTION_GET_CONTENT);
-//        startActivityForResult(intent,createChooser(intent,"Select Image"),PICK_IMAGE_REQUEST);
     }
 }
