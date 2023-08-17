dc489cd07832f2a8a4867d1c08c3393856dcc707
cs21b040 <cs21b040@iittp.ac.in>
2023-08-18 02:37:11 +0530

    "Small Changes"

--- app/src/main/java/com/example/rideshare/ProfileInfo.java
+++ app/src/main/java/com/example/rideshare/ProfileInfo.java
@@ -1,20 +1,12 @@
 package com.example.rideshare;
 
-import static android.content.Intent.createChooser;
-
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContract;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
